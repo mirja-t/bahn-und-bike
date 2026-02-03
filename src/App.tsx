e73a0288e943ac8288c9ae025d8646cc9686d3f8
mirja-t <offensichtbar.mirja@gmail.com>
2026-02-03 17:50:51 +0100

    refactor: rename project and update dependencies
    - Changed project name from "berlin-bahn-bike" to "bahn-und-bike".
    - Updated version to 0.0.0 and set type to module.
    - Replaced react-scripts with Vite for development and build processes.
    - Updated dependencies to their latest versions, including React and Redux.
    - Removed old test file and added a new TypeScript App component.
    - Created main entry point for the application using React Router and Redux.
    - Added TypeScript configuration files for app and node environments.
    - Introduced Vite configuration for building the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,74 @@
-import './App.scss';
-import { Routes, Route, Link, useLocation } from "react-router-dom";
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Home } from './components/home/Home';
-import { Imprint } from './components/imprint/Imprint';
-import { Privacy } from './components/privacy/Privacy';
-import { Container } from './components/container/Container';
-import { Header } from './components/stateless/header/Header';
-import { Footer } from './components/stateless/footer/Footer';
-import { Error } from './components/stateless/error/Error';
-import { Switcher } from './components/form/switcher/Switcher';
+import "./App.scss";
+// import { Routes, Route, Link, useLocation } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Home } from "./components/home/Home";
+// import { Imprint } from "./components/imprint/Imprint";
+// import { Privacy } from "./components/privacy/Privacy";
+// import { Container } from "./components/container/Container";
+// import { Header } from "./components/stateless/header/Header";
+// import { Footer } from "./components/stateless/footer/Footer";
+// import { Error } from "./components/stateless/error/Error";
+// import { Switcher } from "./components/form/switcher/Switcher";
 
-import { 
-  loadLang, 
-  setTheme,
-  selectTheme,
-  selectLangLoading, 
-  selectLangError
-} from './AppSlice';
-import { 
-  setActiveSection,
-  setCurrentTrainroutes,
-  setTrainLinesAlongVeloroute
-} from './components/map/trainroutes/TrainroutesSlice';
-import { 
-  setActiveVeloroute,
-  setActiveVelorouteSection
-} from './components/map/veloroutes/VeloroutesSlice';
-import { Logo } from './components/stateless/header/logo/Logo';
+// import {
+//     loadLang,
+//     setTheme,
+//     selectTheme,
+//     selectLangLoading,
+//     selectLangError,
+// } from "./AppSlice";
+// import {
+//     setActiveSection,
+//     setCurrentTrainroutes,
+//     setTrainLinesAlongVeloroute,
+// } from "./components/map/trainroutes/TrainroutesSlice";
+// import {
+//     setActiveVeloroute,
+//     setActiveVelorouteSection,
+// } from "./components/map/veloroutes/VeloroutesSlice";
+// import { Logo } from "./components/stateless/header/logo/Logo";
 
-function App() {
+export function App() {
+    return "hello world!";
+    /*
+    const [lang, setLang] = useState("de");
+    const theme = useSelector(selectTheme);
+    const [classes, setClasses] = useState("");
+    const dispatch = useDispatch();
+    const languagesLoading = useSelector(selectLangLoading);
+    const languagesError = useSelector(selectLangError);
+    const location = useLocation();
 
-  const [lang, setLang] = useState('de');
-  const theme = useSelector(selectTheme);
-  const [classes, setClasses] = useState('');
-  const dispatch = useDispatch();
-  const languagesLoading = useSelector(selectLangLoading);
-  const languagesError = useSelector(selectLangError);
-  const location = useLocation();
+    const setLanguage = (lang) => {
+        setLang(lang);
+    };
 
-  const setLanguage = lang => {
-    setLang(lang)
-  }
+    const setPageTheme = (theme) => {
+        setTheme(theme);
+        dispatch(setTheme(theme));
+    };
 
-  const setPageTheme = theme => {
-    setTheme(theme);
-    dispatch(setTheme(theme));
-  }
+    useEffect(() => {
+        dispatch(loadLang());
+    }, [dispatch]);
 
-  useEffect(()=>{
-    dispatch(loadLang());
-  },[dispatch]);
+    useEffect(() => {
+        const classNames =
+            location.pathname === "/"
+                ? "home"
+                : location.pathname.match(/[^/]*$/)[0];
+        setClasses(classNames);
+    }, [location]);
 
-  useEffect(()=>{
-    const classNames = location.pathname === "/" ? "home" : location.pathname.match(/[^/]*$/)[0];
-    setClasses(classNames);
-  },[location]);
-
-  const resetState = () => {
-    dispatch(setCurrentTrainroutes([]));
-    dispatch(setActiveSection(null));
-    dispatch(setActiveVeloroute(null));
-    dispatch(setActiveVelorouteSection(null));
-    dispatch(setTrainLinesAlongVeloroute([]));
-  }
-
+    const resetState = () => {
+        dispatch(setCurrentTrainroutes([]));
+        dispatch(setActiveSection(null));
+        dispatch(setActiveVeloroute(null));
+        dispatch(setActiveVelorouteSection(null));
+        dispatch(setTrainLinesAlongVeloroute([]));
+    };
+    
   if(languagesLoading) return <div/>;
   if(languagesError) return <Error/>;
   return (
@@ -120,7 +124,5 @@ function App() {
           <Link to="impressum">{lang==='de' ? 'Impressum' : 'Legal Notes'}</Link>
         </Footer>
       </div>
-    </div>)
+    </div>) */
 }
-
-export default App;
