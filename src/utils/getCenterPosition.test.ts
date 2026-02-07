9a71de32462397257472e3eecc6df1e60854ef4b
mirja-t <offensichtbar.mirja@gmail.com>
2026-02-07 18:56:31 +0100

    chore: update dependencies and refactor tests to use vitest
    - Added vitest as a development dependency in package.json.
    - Refactored getCenterPosition.test.ts to use vitest for assertions.
    - Refactored getLongestDistance.test.ts to use vitest for assertions.

--- src/utils/getCenterPosition.test.ts
+++ src/utils/getCenterPosition.test.ts
@@ -1,16 +1,15 @@
+import { expect, it } from "vitest";
 import { mockedTrainroutes } from "./_testData";
 import { getCenterPosition } from "./getCenterPosition";
-
-const xStart = getCenterPosition(mockedTrainroutes, 'x');
 
 it("returns the avarage middle position of all start positions of a journeys array", () => {
     //arrange
     const trainroutes = mockedTrainroutes;
     const expectedValue = 720;
-    
+
     //act
-    const actualValue = getCenterPosition(trainroutes, 'x');
-    
+    const actualValue = getCenterPosition(trainroutes, "x");
+
     //assert
     expect(actualValue).toEqual(expectedValue);
-  });
+});
--- src/utils/getLongestDistance.test.ts
+++ src/utils/getLongestDistance.test.ts
@@ -1,5 +1,5 @@
+import { expect, it } from "vitest";
 import { mockedTrainroutes } from "./_testData";
-import { getCenterPosition } from "./getCenterPosition";
 import { getLongestDistance } from "./getLongestDistance";
 
 it("returns the highest sum of the subtractions of end positions with avarage start position", () => {
@@ -7,10 +7,10 @@ it("returns the highest sum of the subtractions of end positions with avarage st
     const trainroutes = mockedTrainroutes;
     const xStart = 720;
     const expectedValue = 80;
-    
+
     //act
-    const actualValue = getLongestDistance(trainroutes, 'x', xStart);
-    
+    const actualValue = getLongestDistance(trainroutes, "x", xStart);
+
     //assert
     expect(actualValue).toEqual(expectedValue);
-  });
+});
