753a2514215f33907bdd85eaa6bc80fab1d8b7a2
mirja-t <offensichtbar.mirja@gmail.com>
2026-02-07 18:54:40 +0100

    feat: enhance strict typing for getCenterPosition and getLongestDistance functions

--- src/utils/getCenterPosition.ts
+++ src/utils/getCenterPosition.ts
@@ -1,5 +1,10 @@
+import type { TrainrouteSection } from "../components/map/trainroutes/TrainroutesSlice";
+
 // get the journey center avarage origin on the map
-export const getCenterPosition = (trainroutes: any, coord: "x" | "y") => {
+export const getCenterPosition = (
+    trainroutes: TrainrouteSection[],
+    coord: "x" | "y",
+) => {
     const devider = trainroutes.length > 0 ? trainroutes.length : 1;
     return (
         trainroutes
