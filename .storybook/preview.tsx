f8bf825f8508254d1d983796b0269d14e1f5f6cf
mirja-t <offensichtbar.mirja@gmail.com>
2026-02-08 15:40:37 +0100

    feat: update Storybook configuration and add new stories for Button and Collapse components

---
+++ .storybook/preview.tsx
@@ -0,0 +1,66 @@
+import type { Decorator, Preview } from "@storybook/react-vite";
+import "../src/index.scss";
+import "../src/App.scss";
+import { Story } from "storybook/internal/csf";
+
+export const globalTypes = {
+    theme: {
+        name: "Theme",
+        description: "Global theme for components",
+        defaultValue: "light",
+        toolbar: {
+            icon: "paintbrush",
+            items: [
+                { value: "light", title: "Light" },
+                { value: "dark", title: "Dark" },
+                // add more if you have them:
+                // { value: "solarized", title: "Solarized" },
+            ],
+            dynamicTitle: true,
+        },
+    },
+};
+
+const withTheme: Decorator = (Story, context) => {
+    const theme = context.globals.theme as string;
+
+    const root = document.documentElement; // <html> inside Storybook iframe
+    // remove previous theme-* classes
+    root.className = root.className.replace(/\btheme-\S+/g, "").trim();
+    root.classList.add(`theme-${theme}`);
+
+    // wrap the story
+    return (
+        <div
+            className="App"
+            style={{
+                padding: "3em",
+                display: "flex",
+                justifyContent: "center",
+            }}
+        >
+            <Story />
+        </div>
+    );
+};
+
+const preview: Preview = {
+    decorators: [withTheme],
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+
+        a11y: {
+            // 'todo' - show a11y violations in the test UI only
+            // 'error' - fail CI on a11y violations
+            // 'off' - skip a11y checks entirely
+            test: "todo",
+        },
+    },
+};
+
+export default preview;
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { Button } from "../components/stateless/button/Button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    title: "Stateless/Button",
+    component: Button,
+    decorators: [],
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ["autodocs"],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {
+        label: { control: "text" },
+        type: { control: "select", options: ["button", "submit", "reset"] },
+    },
+    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
+    args: {
+        label: "Button",
+        type: "button",
+    },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+    args: {
+        label: "Button",
+        type: "button",
+    },
+};
