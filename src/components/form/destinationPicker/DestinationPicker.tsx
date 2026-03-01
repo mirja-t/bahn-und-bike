cf43c0d6d5cf1d5a22cbc77472d674324f81b6cc
mirja-t <offensichtbar.mirja@gmail.com>
2026-03-01 15:57:03 +0100

    refactor: Update DestinationPicker to use Redux state for preselected value and enhance city options

--- src/components/form/destinationPicker/DestinationPicker.tsx
+++ src/components/form/destinationPicker/DestinationPicker.tsx
@@ -1,12 +1,17 @@
 import "./destinationpicker.scss";
-import { setStartPos } from "../../map/trainroutes/TrainroutesSlice";
+import {
+    selectStartPos,
+    setStartPos,
+} from "../../map/trainroutes/TrainroutesSlice";
 import { useTranslation } from "../../../utils/i18n";
 import { useAppDispatch } from "../../../AppSlice";
 import { Select } from "../../stateless/select/Select";
+import { useSelector } from "react-redux";
 
 export const DestinationPicker = () => {
-    const dispatch = useAppDispatch();
     const { t } = useTranslation();
+    const dispatch = useAppDispatch();
+    const startPos = useSelector(selectStartPos);
 
     const handleStartChange = (id: string) => {
         dispatch(setStartPos(id));
@@ -18,13 +23,21 @@ export const DestinationPicker = () => {
         { label: "München", value: "609678" },
         { label: "Hameln", value: "513416" },
         { label: "Schwerin", value: "680461" },
+        { label: "Hannover", value: "363803" },
+        { label: "Kassel", value: "249812" },
+        { label: "Düsseldorf", value: "596878" },
+        { label: "Stuttgart", value: "131805" },
+        { label: "Freiburg", value: "378216" },
+        { label: "Frankfurt", value: "632406" },
+        { label: "Erfurt", value: "492446" },
+        { label: "Dresden", value: "243649" },
     ];
 
     return (
         <Select
             options={options}
             name="startdest"
-            preselectedValue={"2975"}
+            preselectedValue={startPos}
             onChange={handleStartChange}
             label={t("startdest")}
         />
