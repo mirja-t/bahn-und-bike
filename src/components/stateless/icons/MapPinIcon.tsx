fc776d2213534258aa5a78a58f38da7f6be1ca65
mirja-t <offensichtbar.mirja@gmail.com>
2026-03-25 10:54:34 +0100

    fix: optimize MapPinIcon rendering and improve animation effects

--- src/components/stateless/icons/MapPinIcon.tsx
+++ src/components/stateless/icons/MapPinIcon.tsx
@@ -21,36 +21,19 @@ export const MapPinIcon = ({
                 initial={{
                     opacity: 0,
                     scale: 1,
-                    y: 0,
                 }}
                 animate={{
                     opacity: active ? 1 : 0,
-                    scale: active ? 1.5 : 1,
-                    y: active ? -2 : 0,
+                    scale: active ? 1.25 / userScale : 1 / userScale,
                 }}
-                d={`M${x + 4.45},${y - 8}c0,-2.7,-2.2,-4.9,-4.9,-4.9s-4.9,2.2,-4.9,4.9s.6,2.6,1.4,3.5c.9.9,3.5,3.4,3.5,3.4,0,0,2.6-2.6,3.5-3.4.9-.9,1.4-2.1,1.4-3.5Z`}
-            />
-            <motion.circle
-                strokeWidth={0.8 / userScale}
-                cx={x - 0.5}
-                cy={y - 10.5}
-                r={5.4 / userScale}
-                fill="var(--bg)"
-                initial={{
-                    opacity: 0,
-                    scale: 1,
-                }}
-                animate={{
-                    opacity: active ? 1 : 0,
-                    scale: active ? 1 : 0,
-                }}
+                d={`M${x + 4.45},${y - 8 / userScale}c0,-2.7,-2.2,-4.9,-4.9,-4.9s-4.9,2.2,-4.9,4.9s.6,2.6,1.4,3.5c.9.9,3.5,3.4,3.5,3.4,0,0,2.6-2.6,3.5-3.4.9-.9,1.4-2.1,1.4-3.5Z`}
             />
             <motion.text
-                x={x}
-                y={y - 7}
+                x={x - 1 / userScale}
+                y={y - 5 / userScale}
                 textAnchor="middle"
-                fontSize={8 / userScale}
-                fill={`var(--text)`}
+                fontSize={7 / userScale}
+                fill={`var(--bg)`}
                 style={{ pointerEvents: "none" }}
                 fontFamily="Oswald, sans-serif"
                 fontWeight={600}
