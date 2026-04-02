2ae0bfa8c9c5a650f1a7eceee74551ec04598a7c
mirja-t <offensichtbar.mirja@gmail.com>
2026-04-02 17:55:02 +0200

    fix: update map component to manage offsets and improve drag functionality

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -19,23 +19,33 @@ import {
     selectAppZoom,
 } from "../../AppSlice";
 import { selectVeloroutesLoading } from "./veloroutes/VeloroutesSlice";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface MapProps {
     value: number;
 }
 export const Map = ({ value }: MapProps) => {
     const mapWrapperRef = useRef<HTMLDivElement | null>(null);
+    const mapContainerRef = useRef<HTMLDivElement | null>(null);
     const resetKey = useSelector(selectResetKey);
     const journeys = useSelector(selectCurrentTrainroutes);
     const isLoading = useSelector(selectTrainrouteListLoading);
     const veloroutesLoading = useSelector(selectVeloroutesLoading);
     const appZoom = useSelector(selectAppZoom);
     const dispatch = useAppDispatch();
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
+    const [cachedOffset, setCachedOffset] = useState({ x: 0, y: 0 });
 
     const handleMapZoom = (dir: "+" | "-") => {
         const factor = dir === "+" ? 2 : 0.5;
         dispatch(setUserScale(factor));
+        setOffset((prev) => ({
+            x: prev.x + cachedOffset.x,
+            y: prev.y + cachedOffset.y,
+        }));
+        drag.x.set(0);
+        drag.y.set(0);
+        setCachedOffset({ x: 0, y: 0 });
     };
 
     const zoom = useZoom(journeys, Number(value), isLoading);
@@ -48,6 +58,25 @@ export const Map = ({ value }: MapProps) => {
     useEffect(() => {
         dispatch(setAppScale(zoom.scale));
     }, [dispatch, zoom]);
+
+    const handleDragEnd = () => {
+        const offsetX =
+            drag.x.get() /
+            (mapContainerRef.current?.offsetWidth || 0) /
+            appZoom;
+        const offsetY =
+            drag.y.get() /
+            (mapContainerRef.current?.offsetHeight || 0) /
+            appZoom;
+        setCachedOffset({ x: offsetX, y: offsetY });
+    };
+
+    useEffect(() => {
+        setOffset({
+            x: zoom.x,
+            y: zoom.y,
+        });
+    }, [zoom]);
 
     return (
         <div ref={mapWrapperRef} className={styles.mapWrapper}>
@@ -65,11 +94,12 @@ export const Map = ({ value }: MapProps) => {
                     width: 100 * Math.min(containerRatio, 1) + "%",
                 }}
             >
-                <div className={styles.mapContainer}>
+                <div className={styles.mapContainer} ref={mapContainerRef}>
                     <motion.div
                         className={styles.mapInnerContainer}
                         drag
                         dragMomentum={false}
+                        onDragEnd={handleDragEnd}
                         style={{
                             x: drag.x,
                             y: drag.y,
@@ -78,7 +108,7 @@ export const Map = ({ value }: MapProps) => {
                         <div
                             className={styles.map}
                             style={{
-                                transform: `scale(${appZoom}) translate(${zoom.x * 100}%, ${zoom.y * 100}%)`,
+                                transform: `scale(${appZoom}) translate(${offset.x * 100}%, ${offset.y * 100}%)`,
                             }}
                         >
                             {!isLoading && <Trainroutes />}
