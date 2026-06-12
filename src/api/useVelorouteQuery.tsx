5be3e427affb8e5d79cefb334a63c5fbc5551579
mirja-t <offensichtbar.mirja@gmail.com>
2026-06-12 15:51:06 +0200

    feat: add useVelorouteQuery hook for fetching veloroute data

---
+++ src/api/useVelorouteQuery.tsx
@@ -0,0 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import type { UseQueryResult } from "@tanstack/react-query";
+import type {
+    Veloroute,
+    VeloroutesResponseStop,
+} from "@/components/map/veloroutes/VeloroutesSlice";
+import { headers, VITE_API_URL } from "@/config/config";
+import { convertVelorouteStops, makeVeloRoute } from "@/utils/makeVeloRoute";
+
+type QueryParams = {
+    id: string;
+    trainstops: number[];
+    maxDistToNextStation: number;
+};
+export type VelorouteQueryParamsType = QueryParams | null;
+
+const fetchVeloroute = async (queryParams: VelorouteQueryParamsType) => {
+    if (!queryParams?.id) throw new Error("Missing query params");
+    const { id, trainstops, maxDistToNextStation } = queryParams;
+    const responseStops: VeloroutesResponseStop[] = await fetch(
+        `${VITE_API_URL}veloroute/${id}`,
+        { headers },
+    ).then((res) => {
+        if (res.status !== 200) throw new Error("Bad Server Response");
+        return res.json();
+    });
+    const velorouteStops = convertVelorouteStops(responseStops, trainstops);
+    return makeVeloRoute(
+        velorouteStops,
+        maxDistToNextStation,
+        id,
+        responseStops[0].name,
+    );
+};
+
+export function useVelorouteQuery(
+    queryParams: VelorouteQueryParamsType,
+): UseQueryResult<Veloroute> {
+    const id = queryParams?.id;
+    const trainstops = queryParams?.trainstops;
+    const maxDistToNextStation = queryParams?.maxDistToNextStation;
+    return useQuery({
+        queryKey: ["veloroute", id, trainstops, maxDistToNextStation],
+        queryFn: () => fetchVeloroute(queryParams),
+        enabled: queryParams !== null && !!id,
+        keepPreviousData: true,
+        staleTime: 10 * 60 * 1000, // 10 minutes
+    });
+}
